d9d9c968a1d466a5db10c73f24331d52a6373087
BenjyCabrera <benjamin.cabrera.arroyo89@gmail.com>
2023-06-07 14:47:54 +0200

    ignorados env y node

--- server/src/app.js
+++ server/src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const mysql = require("mysql2");
 const bodyParser = require("body-parser");
-const corsss = require("cors");
+const cors = require("cors");
 require('dotenv').config();
 
 // RUTAS ----------------------------------
