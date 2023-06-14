1973655f584fbea371328b644cb31a62187d777d
BenjyCabrera <benjamin.cabrera.arroyo89@gmail.com>
2023-06-14 13:01:40 +0200

    correo implementado webservice

--- server/src/app.js
+++ server/src/app.js
@@ -3,10 +3,36 @@ const mysql = require("mysql2");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 require('dotenv').config();
-
+const nodemailer = require("nodemailer");
+const cron = require('node-cron');
+const pool = require("./mysqlPool");
 // RUTAS ----------------------------------
 const employeesRouter = require("./routes/employees");
 const usersRouter = require("./routes/users");
+
+
+let transporter = nodemailer.createTransport({
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false, // true for 465, false for other ports
+  auth: {
+    user: "vivapharmahr@gmail.com", // generated ethereal user
+    pass: process.env.email_key // generated ethereal password
+  }
+});
+
+async function sendEmailo(email, body) {
+ 
+  let info = await transporter.sendMail({
+    from: '"VivaPharma.HR" <vivapharmahr@gmail.com>', // sender address
+    to: email,
+    subject: "Notificaciones VivaPharma.HR", // Subject line
+    text: body, // plain text body
+    //html: body, // html body
+  });
+  
+} 
+
 
 const app = express();
 app.set("port", process.env.PORT || 4000);
@@ -16,6 +42,35 @@ app.use(cors());
 
 app.use("/employees", employeesRouter);
 app.use("/users", usersRouter);
+
+
+
+
+
+
+// Esta función envia a un correo que pasas como arguemnto
+
+
+
+// Cada cinco segundos envia un correo a los que tienen la Notificación 2 activa
+cron.schedule('* */5 * * * *', () => {
+
+  async function sendNotificacione1() {
+    const [rows] = await pool.query("SELECT email FROM registro WHERE Notificacion2 = ?", [1]);
+
+
+    rows.forEach(u => {
+    
+      sendEmailo(u.email, "Esta es una notificación muy importante")
+    })
+  }
+
+  sendNotificacione1()
+
+
+  console.log('running a task every minute');
+}).start()
+
 
 // SERVER RUNNING--------------------------
 app.listen(app.get("port"), () => {
--- server/src/routes/users.js
+++ server/src/routes/users.js
@@ -5,7 +5,7 @@ const pool = require("../mysqlPool");
 const jwt = require("jsonwebtoken");
 const { body, validationResult } = require("express-validator");
 const bodyParser = require("body-parser");
-
+const isAuth = require("../middlewares/isAuth");
 // Validación de datos para la ruta '/signup'
 const validateSignupData = [
   body("name").notEmpty().withMessage("El nombre es obligatorio"),
@@ -19,79 +19,140 @@ const validateSigninData = [
   body("password").notEmpty().withMessage("La contraseña es obligatoria"),
 ];
 
-// SIGNUP
-router.post("/signup", bodyParser.json(), validateSignupData, async (req, res) => {
-  const { name, password: passwordPlainText, email } = req.body;
+// GET ALL USER//////////
 
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    return res.status(400).json({ errors: errors.array() });
-  }
-
+router.get("/", async (req, res) => {
   try {
-    const salt = await bcrypt.genSalt(10);
-    const password = await bcrypt.hash(passwordPlainText, salt);
-
-    const sql =
-      "INSERT INTO prueba.registro (usuario, contrasena, email) VALUES (?, ?, ?)";
-    await pool.query(sql, [name, password, email]);
-
-    const newUser = {
-      name,
-      email,
-    };
-
-    const token = jwt.sign(newUser, process.env.jwt_privateKey);
-
+    const sql = "SELECT * FROM prueba.registro";
+    const [rows] = await pool.query(sql);
     res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("access-control-expose-headers", "x-auth-token");
-    res.setHeader("x-auth-token", token).json(newUser);
+    res.json(rows);
   } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: "Error al registrar el usuario" });
+    console.error("Error al ejecutar la consulta SQL:", error);
+    res.status(500).json({ error: "Error al obtener los datos" });
   }
 });
 
+// SIGNUP
+router.post(
+  "/signup",
+  bodyParser.json(),
+  validateSignupData,
+  async (req, res) => {
+    const { name, password: passwordPlainText, email } = req.body;
+
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+      const salt = await bcrypt.genSalt(10);
+      const password = await bcrypt.hash(passwordPlainText, salt);
+
+      const sql =
+        "INSERT INTO prueba.registro (usuario, contrasena, email) VALUES (?, ?, ?)";
+      await pool.query(sql, [name, password, email]);
+
+      const newUser = {
+        name,
+        email,
+      };
+
+      const token = jwt.sign(newUser, process.env.jwt_privateKey);
+
+      res.setHeader("Access-Control-Allow-Origin", "*");
+      res.setHeader("access-control-expose-headers", "x-auth-token");
+      res.setHeader("x-auth-token", token).json(newUser);
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: "Error al registrar el usuario" });
+    }
+  }
+);
+
 // SIGNIN
-router.post("/signin", bodyParser.json(), validateSigninData, async (req, res) => {
-  const { email, password: passwordPlainText } = req.body;
+router.post(
+  "/signin",
+  bodyParser.json(),
+  validateSigninData,
+  async (req, res) => {
+    const { email, password: passwordPlainText } = req.body;
 
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    return res.status(400).json({ errors: errors.array() });
-  }
-
-  try {
-    const [rows] = await pool.query("SELECT * FROM prueba.registro WHERE email = ?", [email]);
-
-    const user = rows[0];
-
-    if (!user) {
-      return res.status(401).json({ message: "Usuario o contraseña incorrecta" });
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
     }
 
-    const isUser = await bcrypt.compare(passwordPlainText, user.contrasena);
-    if (!isUser) {
-      return res.status(401).json({ message: "Usuario o contraseña incorrecta" });
+    try {
+      const [rows] = await pool.query(
+        "SELECT * FROM prueba.registro WHERE email = ?",
+        [email]
+      );
+
+      const user = rows[0];
+
+      if (!user) {
+        return res
+          .status(401)
+          .json({ message: "Usuario o contraseña incorrecta" });
+      }
+
+      const isUser = await bcrypt.compare(passwordPlainText, user.contrasena);
+      if (!isUser) {
+        return res
+          .status(401)
+          .json({ message: "Usuario o contraseña incorrecta" });
+      }
+
+      const token = jwt.sign(
+        { email: user.email, id: user.id },
+        process.env.jwt_privateKey
+      );
+
+      res.setHeader("Access-Control-Allow-Origin", "*");
+      res.setHeader(
+        "Access-Control-Allow-Methods",
+        "GET, POST, PATCH, PUT, DELETE, OPTIONS"
+      );
+      res.setHeader(
+        "Access-Control-Allow-Headers",
+        "Origin, Content-Type, X-Auth-Token"
+      );
+      res.setHeader("Access-Control-Expose-Headers", "X-Auth-Token");
+      res.setHeader("x-auth-token", token);
+
+      res.json({ message: "Inicio de sesión exitoso" });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: "Error en el inicio de sesión" });
     }
-
-    const token = jwt.sign({ email: user.email }, process.env.jwt_privateKey);
-
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, PUT, DELETE, OPTIONS");
-    res.setHeader("Access-Control-Allow-Headers", "Origin, Content-Type, X-Auth-Token");
-    res.setHeader("Access-Control-Expose-Headers", "X-Auth-Token");
-    res.setHeader("x-auth-token", token);
-
-    res.json({ message: "Inicio de sesión exitoso" });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: "Error en el inicio de sesión" });
   }
+);
+
+// PUT////////
+
+router.put("/settings", isAuth, bodyParser.json(), async (req, res) => {
+  console.log(req.body);
+
+  console.log(req.user);
+
+  const { notification1, notification2, notification3 } = req.body;
+
+  const [rows] = await pool.query(
+    "UPDATE registro SET Notificacion1 = ?, Notificacion2 = ?, Notificacion3 = ? WHERE id = ?",
+    [notification1, notification2, notification3, req.user.id]
+  );
+});
+
+// GET USER DATA////////
+
+router.get("/account", isAuth, bodyParser.json(), async (req, res) => {
+  const [rows] = await pool.query("SELECT * FROM registro WHERE id = ?", [
+    req.user.id,
+  ]);
+
+  res.status(200).json(rows[0]);
 });
 
 module.exports = router;
-
-
-
-
